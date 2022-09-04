c35428e84a525e61e736ca679f88a5d489888c9a
Giang-1998 <huyenthoaicuatheky@gmail.com>
2022-09-04 22:47:24 +0700

    22h 50 4/9/22

---
+++ src/redux/reducer.js
@@ -0,0 +1,27 @@
+const initState = {
+    filters: {
+        search: '',
+        status: 'All',
+        priority: []
+    },
+    todoList: [
+        {id: 1, name: 'Learn Yoga', completed:false, priority: 'Medium'},
+        {id: 2, name: 'Learn Reactjs', completed:true, priority: 'Hight'},
+        {id: 3, name: 'Learn Javascript', completed:false, priority: 'Low'}
+    ]
+};
+const rootReducer = (state = initState, action) => {
+    switch (action.type){
+        case 'todolist/todo': 
+        return {
+            ...state,
+            todoList:[
+                ...state.todoList,
+                {id: 4, name:'Learn Css', completed: true, priority: 'Medium'}
+            ]
+        };
+        default: return state;
+    }
+}
+
+export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { legacy_createStore as createStore } from 'redux';
+import rootReducer from './reducer';
 
 const store = createStore(rootReducer);
 
